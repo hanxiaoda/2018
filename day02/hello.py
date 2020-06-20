465495ce6d376920fdbe8f996b462aa28a781e06
Tedu <784419177@qq.com>
2020-06-20 12:13:39 +0800

    add files

---
+++ day02/hello.py
@@ -0,0 +1,6 @@
+sum = 0
+num = 1
+while num<101:
+    sum += num
+    num +=1
+print(sum)
---
+++ day03/file02.py
@@ -0,0 +1,8 @@
+
+#f = open("exercise.txt","a")
+f = open("exercise.txt","r")
+#f = open("exercise.txt","r")
+for line in f:
+    
+    print(line) 
+f.close
